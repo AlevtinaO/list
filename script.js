40026bee2dea4715e0ee8c71fe079661b30f1893
olkhovskikh@bk.ru <olkhovskikh@bk.ru>
2022-12-08 23:54:29 +0300

    value

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let myTitle = document.getElementById ("title")
+let count=0
+function addTask () {
+  let title = document.getElementById ("title")  
+  let ol = document.getElementById ("ol") 
+  let input = document.getElementById ("input")
+  let value = input.value
+
+  count = count+1
+  ol.innerHTML = ol.innerHTML + "<li>" + value + "</li>" 
+}
+ 
+
+
