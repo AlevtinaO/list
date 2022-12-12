1020f2c4a85e9e08deb3443da6fdee56269e1371
olkhovskikh@bk.ru <olkhovskikh@bk.ru>
2022-12-12 20:30:32 +0300

    правка

--- script.js
+++ script.js
@@ -4,10 +4,25 @@ function addTask () {
   let ol = document.getElementById ("ol") 
   let input = document.getElementById ("input")
   let value = input.value
+  let error = document.getElementById ("error") 
 
   count = count+1
   ol.innerHTML = ol.innerHTML + "<li>" + value + "</li>" 
+  if (count>5) {
+    error.innerHTML = "Можно ввести не больше 5 дел!" 
+  }
 }
- 
+  
+
+
+let username = prompt ("Введите своё имя:")
+let title = document.getElementById ("title")
+if(username) {
+  title.innerHTML = "Ваш список дел," +username + ":"
+}
+else {
+  title.innerHTML = "Ваш список дел, анонимный пользователь!"
+}
+
 
 
